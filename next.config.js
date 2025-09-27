85ade2e04440f1478fced1226a81206f630bb5f5
Suonty2 <suontii2023@gmail.com>
2025-09-27 13:49:34 +0700

    Push

--- next.config.js
+++ next.config.js
@@ -3,6 +3,15 @@ const nextConfig = {
   output: 'export',
   basePath: '/dev-ty', // <-- your repo name
   assetPrefix: '/dev-ty/', // <-- ensures images and assets load correctly
+
 }
 
+
 module.exports = nextConfig
+
+  distDir: 'out',
+  // basePath: '/suon-ty-portfolio-next-js',
+  // assetPrefix: '/suon-ty-portfolio-next-js/',
+  images: {
+    unoptimized: true,
+  },
