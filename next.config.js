54b483eba6806a9c37bcf25dd830a0f0b42b35e8
Suonty2 <suontii2023@gmail.com>
2025-09-27 14:59:56 +0700

    Push

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
-  basePath: '/dev-ty',       // repo name, no trailing slash
-  assetPrefix: '/dev-ty/',   // with trailing slash is fine here
-  images: {
-    unoptimized: true,       // required for next/image with `next export`
-  },
+    output: 'export',
+    basePath: '/dev-ty',     // repo name, no trailing slash
+    assetPrefix: '/dev-ty/', // ensures assets load from repo path
+    images: {
+        unoptimized: true,     // required for next/image with static export
+    },
 }
 
-module.exports = nextConfig;
+
+
+module.exports = nextConfig
